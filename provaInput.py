75a80b84e155aaec5aac21b359fd274035480b7e
Giorgioboh <giorgiocucchi30@gmail.com>
2024-11-26 11:54:59 +0100

    proveInput

---
+++ provaInput.py
@@ -0,0 +1,5 @@
+nomeInput = input("Inserisci il tuo nome: ")
+cognomeInput = input("Inserisci il tuo cognome: ")
+etaInput = int(input("Inserisci la tua età: "))
+
+print ("Nel 2050 avrai: ", etaInput + 26, "anni")
