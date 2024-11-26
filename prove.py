fef627cecefacf5ce94792be37933a525668e0cd
Giorgioboh <giorgiocucchi30@gmail.com>
2024-11-26 11:38:20 +0100

    modifica codice e cancellazione commento

--- prove.py
+++ prove.py
@@ -1,2 +1,16 @@
-print("Hello World")
-#commento di test
+i = 5
+k = 0
+
+while i == 5:
+    import random
+    num1 = random.randint(0, 100)
+    num2 = random.randint(0, 100)
+    k += 1
+    if num1 != num2:
+        print(num1, num2)
+
+    else:
+        print(num1, "=", num2)
+        i = 3
+
+print("ci sono voluti" ,k ,"tentativi")
